a4514aad6893be4c81f4a1a427076bb547cdc89b
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-13 09:54:38 +0530

    router setup done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,35 @@
-import Home from "./pages/home";
+import { Suspense, lazy } from "react";
+import {
+  Outlet,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const Home = lazy(() => import("./pages/home"));
+const Cart = lazy(() => import("./pages/cart"));
+const Profile = lazy(() => import("./pages/profile"));
+const ProductDetails = lazy(() => import("./pages/productDetails"));
 
 function App() {
   return (
     <main className="bg-gray-100">
-      <Home />
+      <Suspense fallback={"loading..."}>
+        <Outlet />
+      </Suspense>
     </main>
   );
 }
 
 export default App;
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="productDetails" element={<ProductDetails />} />
+    </Route>
+  )
+);
