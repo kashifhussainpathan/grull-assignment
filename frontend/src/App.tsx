87f0017cdd53132392536d256e379eb6f02f194e
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-13 09:37:28 +0530

    frontend project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <main className="bg-gray-100">
+      <Home />
+    </main>
+  );
+}
+
+export default App;
---
+++ frontend/src/types/data.types.ts
@@ -0,0 +1,12 @@
+export interface IProduct {
+  id: string;
+  name: string;
+  description: string;
+  brand: string;
+  gender: string;
+  image_urls: string[];
+  price: number;
+  color: string;
+  product_type: string;
+  units_in_stock: number;
+}
